1ea27033533392c12ed215a12378a7f2a3443c92
KonkenBonken <konrad05pettersson@gmail.com>
2022-03-14 17:13:59 +0100

    Beautified

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
 <?php
-	header("Link:" .
+header("Link:" .
 	"<https://fonts.gstatic.com>; rel=preconnect," .
 	"<img/gurka.png>; rel=prefetch," .
 	"<img/moon.png>; rel=prefetch," .
 	"<img/sun.png>; rel=prefetch"
-	);
+);
 ?>
 
 	<head>
@@ -15,17 +15,15 @@
 		<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 		<script defer src="app.js"></script>
 		<link href="styles.css" rel="stylesheet">
-		<?php if ( isset($_REQUEST['allergisk']) ) {
-			echo	"<style>#gurka{display:none}</style>";
-		} ?>
+		<?=isset($_REQUEST['allergisk'])) ? '<style>#gurka{display:none}</style>' : '' ?>
 	</head>
-	<body>
+	<body <?=isset($_REQUEST['darkmode']) ? 'class="darkmode"' : '' ?>>
 			<img src="img/gurka.png" id="gurka"></img>
 		<ul>
 			<li id="mail"><a href="mailto:tim@gurka.se" target="_blank" rel="noopener noreferrer">tim@gurka.se</a></li>
 			<li id="turns" title="Varv snurrade">0</li>
 			<li id="tpm" title="Varv per sekund">0</li>
-					</ul>
+		</ul>
 		<img id="darkmodeButton" src="img/moon.png" alt="Dark-mode"  title="Dark-mode">
 	</body>
 </html>
